ced26c9db12e3da09e45edddcbbe5db39b8867e6
Gydra <102178372+otmorozika17101978@users.noreply.github.com>
2023-09-22 01:56:43 +0300

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,64 @@
+    main()
+
+def main():
+import random
+    data = generate_random_data()
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+
+    return data
+    for item in data:
+
+if __name__ == "__main__":
+        print(f"Random Number: {item}")    return data
+def generate_random_data():
+    main()
+    for item in data:
+
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+def main():
+
+    data = generate_random_data()
+
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+        }
+}
+            System.out.println("Random Number: " + item);
+        for (int item : data) {
+        int[] data = new int[10];
+    public static void main(String[] args) {
+
+    }
+public class RandomDataGenerator {
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+        }
+    public static void main(String[] args) {
+
+
+    }
+        }
+public class RandomDataGenerator {
+        for (int item : data) {
+        for (int i = 0; i < 10; i++) {    for item in data:
+        print(f"Random Number: {item}")
+
+
+
+
+    data = generate_random_data()
+    return data
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+if __name__ == "__main__":
+import random
+def generate_random_data():
